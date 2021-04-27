bd13108ee5dda04f1354a922ec9260f1480da6b7
Dawar Aamir <dawar.aamir@gmail.com>
2021-04-27 17:49:35 -0400

    Movie SQL done

---
+++ src/main/resources/data.sql
@@ -0,0 +1,30 @@
+INSERT INTO homes (address, homenumber) VALUES ('36 E. Bayberry Rd.Savannah, GA 31404', '565-6895');
+INSERT INTO homes (address, homenumber) VALUES ('11 Essex Dr.Farmingdale, NY 11735', '454-4544');
+INSERT INTO homes (address, homenumber) VALUES ('920 Arlington Street Clifton, NJ 07011', '985-4515');
+INSERT INTO homes (address, homenumber) VALUES ('234 High Street, PA 19159 ', '267-3940');
+
+
+INSERT INTO people (last_name, first_name, mobile, birthday, home_id)
+VALUES ('Carbral', 'Sheeri', '230-4233', '1970-02-23', 2);
+INSERT INTO people (last_name, first_name, mobile, birthday, home_id)
+VALUES ('Sharam', 'Raj', '186-5223', '1980-08-31', 3);
+INSERT INTO people (last_name, first_name, mobile, birthday, home_id)
+VALUES ('Durand', 'Noelle', '395-6161', '1960-07-06', 1);
+INSERT INTO people (last_name, first_name, mobile, birthday, home_id)
+VALUES ('Smith', 'Thomas', '395-6181', '1987-07-06', 1);
+INSERT INTO people (last_name, first_name, mobile, birthday, home_id)
+VALUES ('Smith', 'Jane', '393-6181', '1987-12-06', 3);
+INSERT INTO people (last_name, first_name, mobile, birthday, home_id)
+VALUES ('Brown', 'Doug', '466-6241', '1954-12-07', 3);
+
+
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Howard the Duck', 110, 'Sci-Fi', 4.6, 'PG'),
+('Lavalantula', 83, 'Horror', 4.7, 'TV-14'),
+('Starship Troopers', 129, 'Sci-Fi', 7.2, 'PG-13'),
+('Waltz With Bashir', 90, 'Documentary', 8.0, 'R'),
+('Spaceballs', 96, 'Comedy', 7.1, 'PG'),
+('Monsters Inc.', 92, 'Animation', 8.1 , 'G');
+
+
+
---
+++ src/main/resources/moviesScript.sql
@@ -0,0 +1,40 @@
+SELECT *
+FROM movies
+WHERE genre = 'Sci-Fi';
+
+SELECT *
+FROM movies
+WHERE imdb_score >= 6.5;
+
+SELECT *
+FROM movies
+WHERE rating LIKE '%G' AND runtime < 100;
+
+SELECT genre, AVG(runtime)
+FROM movies WHERE imdb_score < 7.5
+GROUP BY genre;
+
+UPDATE movies
+SET rating = 'R'
+WHERE title = 'Starship Troopers';
+-- To check the change
+SELECT * FROM MOVIES;
+
+SELECT id, rating
+FROM movies
+WHERE genre = 'Horror' OR genre = 'Documentary';
+
+SELECT rating, AVG(imdb_score), MAX(imdb_score), MIN(imdb_score)
+FROM movies
+GROUP BY rating;
+
+SELECT rating, AVG(imdb_score), MAX(imdb_score), MIN(imdb_score)
+FROM movies
+GROUP BY rating
+HAVING COUNT(*) > 1;
+
+DELETE
+FROM movies
+WHERE rating = 'R';
+-- To check the change
+SELECT * FROM MOVIES;
